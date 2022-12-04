8f34757374ac32fae5b62f36075c144092065e33
vandenbroecksebastiaan <van.den.broeck.sebastiaan@gmail.com>
2022-12-04 19:04:20 +0100

    refactoring

--- bankruptcy.py
+++ bankruptcy.py
@@ -1,216 +1,216 @@
 import pandas as pd
 import numpy as np
 
-# - Company name
-# - Flags
-# - Last avail. year (independent censoring?)
 
-# - Postcode
-# - City
-# - Region in country
-
-# - BvD sectors
-# - Specialisation
-
-# - Peer group name
-# - Peer group size
-
-# - Legal events - Date
-# - Legal events - Description
-# - Legal events - Type
-
-# - Operating revenue [-5:]
-# - Cash flow [-5:]
-# - Total assets [-5:]
-# - Profit margin [-5:]
-# - Number of employees [-5:]
-
-# - Status
-# - Status date
-# - Local status
-# - Local status date
+class BankruptcyDataset:
+    def __init__(self, path):
+        self.df = self._read_df(path)
 
 
-def read_df(path):
-    df = pd.read_csv(path, delimiter="\t", encoding="UTF-16 LE")
-    df = df[df["Mark"].notna()]
-    # df["Status"] = df["Status"].astype(str)
-    # df["Status date"] = df["Status date"].astype(str)
-    return df
+    def create_transf_df(self):
+        """Transforsm the raw data from Bel-First to a dataset that is suitable
+        for further analysis."""
+        self.transf_df = pd.DataFrame(index=self.df.index.unique())
+        self.transf_df.index = self.transf_df.index.rename("index")
+        self._add_status()
+        self._add_status_indicator()
+
+        # Only keep companies with the following status
+        self.transf_df = self.transf_df[self.transf_df["status"].isin(
+            ["Liquidation", "Bankruptcy", "Merger by absorption"]
+        )]
+    
+        self._add_mean_pl()
+        self._add_mean_total_assets()
+        self._add_mean_n_employees()
+        self._add_legal_form()
 
 
-def fill_index(df):
-    df["Mark"] = df["Mark"].fillna(method="ffill")
-    df = df.set_index("Mark")
-    return df
+    def _read_df(self, path):
+        df = pd.read_csv(path, delimiter="\t", encoding="UTF-16 LE")
+        df = df[df["Mark"].notna()]
+        df["Mark"] = df["Mark"].fillna(method="ffill")
+        df = df.set_index("Mark")
+        return df
+    
+    
+    def _add_status(self):
+        """Adds a status, for example bankruptcy, to the transformed dataframe.
+        It also calculates a lower bound on the years to that status."""
+        # Add status to new df
+        self.transf_df["status"] = self.df["Legal situation"]
+        replace_map = {"Conclusion of liquidation": "Liquidation",
+                       "Reopening liquidation": "Liquidation",
+                       "Conclusion of bankruptcy": "Bankruptcy"}
+        self.transf_df["status"] = self.transf_df["status"].replace(replace_map)
+    
+        # Calculate years to event
+        self.transf_df["end_date"] = self.df["Legal situation date"]
+        self.transf_df["start_date"] = self.df["Date of incorporation"]
+    
+        self.transf_df["end_date"] = pd.to_datetime(
+            self.transf_df["end_date"], dayfirst=True,
+        )
+        self.transf_df["start_date"] = pd.to_datetime(
+            self.transf_df["start_date"], dayfirst=True,
+        )
+    
+        self.transf_df["years_to_event"] = [
+            len(pd.date_range(start=i, end=j, freq="Y"))
+            for i, j in zip(self.transf_df["start_date"],
+                            self.transf_df["end_date"])
+        ]
+        self.transf_df["years_to_event"] = self.transf_df["years_to_event"]\
+            .astype(int)
+    
+    
+    def _add_status_indicator(self):
+        """Adds a new column to the transformed dataframe which contains a
+        unique int value for each status."""
+        indicator_map = self.transf_df["status"].unique().tolist()
+        indicator_map = {
+            i:j for i, j in
+            zip(indicator_map, list(range(len(indicator_map))))
+        }
+    
+        self.transf_df["event_indicator"] = [
+            indicator_map[i] for i in self.transf_df["status"]
+        ]
+    
+    
+    def _add_mean_pl(self):
+        """Adds the mean profit loss in thousands of all profit loss columns
+        contained in the original dataframe."""
+        cols = [i for i in self.df.columns if "P/L" in i]
+        self.df[cols] = self.df[cols].replace("n.a.", np.nan)
+        self.transf_df["mean_pl"] = self.df[cols].fillna(0).mean(axis=1)
+        self.transf_df["mean_pl"] = self.transf_df["mean_pl"] / 1000
+    
+    
+    def _add_mean_cash_flow(self):
+        """Adds the mean cash flow of all cash flow columns
+        contained in the original dataframe."""
+        cols = [i for i in self.df.columns if "Cash flow" in i]
+        self.df[cols] = self.df[cols].replace("n.a.", np.nan)
+        self.transf_df["mean_cash_flow"] = self.df[cols].fillna(0).mean(axis=1)
+    
+    
+    def _add_mean_total_assets(self):
+        """Adds the mean total assets of all total asset columns
+        contained in the original dataframe."""
+        cols = [i for i in self.df.columns if "Total assets" in i]
+        self.df[cols] = self.df[cols].replace("n.a.", np.nan)
+        self.transf_df["mean_total_assets"] = self.df[cols].fillna(0).mean(axis=1)
+        self.transf_df["mean_total_assets"] = self.transf_df["mean_total_assets"]\
+            / 1000
+    
+    
+    def _add_mean_profit_margin(self):
+        """Adds the mean profit margin of all profit margin columns
+        contained in the original dataframe."""
+        cols = [i for i in self.df.columns if "Profit margin" in i]
+        self.df[cols] = self.df[cols].replace("n.a.", np.nan)
+        self.transf_df["mean_profit_margin"] = self.df[cols].fillna(0).mean(axis=1)
+    
+    
+    def _add_mean_n_employees(self):
+        """Adds the mean number of employees of all relevant columns
+        contained in the original dataframe."""
+        cols = [i for i in self.df.columns if "employees" in i]
+        self.df[cols] = self.df[cols].replace("n.a.", np.nan)
+        self.transf_df["mean_n_employees"] = self.df[cols].fillna(0).mean(axis=1)
+    
+    
+    def add_n_legal_events(self):
+        """Adds the number of legal events of the entire lifetime of the
+        company."""
+        self.transf_df["n_legal_events"] = self.df["Legal events - Date"]\
+            .str.count("\n")
+        self.transf_df["n_legal_events"] = self.transf_df["n_legal_events"].fillna(0)
+        self.transf_df["n_legal_events"] = self.transf_df["n_legal_events"] + 1
+    
+    
+    def _add_peer_group_size(self):
+        self.transf_df["peer_group_size"] = self.df["Peer Group Size"]
+        return self.transf_df
+    
+    
+    def _add_legal_form(self):
+        """Adds a column legal form to the transformed dataframe which
+        indicates whether the company is public, private, cooperative, nonprofit
+        or of another type."""
+        private_list = [
+            "Private company with limited liability", "private_company",
+            "Private company limited by shares",
+            "Starter Private company with limited liability",
+            "Limited partnership", "Partnership limited by shares",
+            "General partnership", "One-man company with limited liability",
+            "Private company with limited liability with a social aim",
+            "Private Founding",
+            "Professional partnership in the form of a private company with "\
+            "limited liability",
+            "Private company"
+        ]
+        public_list = [
+            "Company limited by shares",
+            "Public company limited by shares",
+            "Public company of another legal form",
+            "Public utility Founding",
+            "Public institution"
+        ]
+        cooperative_list = [
+            "Co-operative society with limited liability",
+            "Co-operative society with limited liability with a social aim",
+            "Co-operative society",
+            "Co-operative society with unlimited joint and several liability",
+            "Co-operative society with limited liability by way of "\
+            "participating interest",
+            "Public co-operative society"
+        ]
+        nonprofit_list = [
+            "Non-profit association", "Private non-profit association",
+            "Association in charge of missions",
+            "International non-profit association",
+            "Company limited by shares with a social aim"
+        ]
+        other_list = [
+            "Foreign company whose nature is not described",
+            "Provincial government control",
+            "Project Association",
+            "Autonomous local authority with legal personality",
+            "Economic joint venture with Belgian office",
+            "European economic joint venture with Belgian office",
+            "Public utility founding"
+        ]
+        categories =  [private_list, public_list, cooperative_list,
+    
+                       nonprofit_list, other_list]
+        labels = ["private", "public", "cooperative", "nonprofit", "other"]
+    
+        remap = {}
+        for i, j in zip(categories, labels):
+            for k in i:
+                remap[k] = j
+    
+        self.df["Legal form"] = self.df["Legal form"].replace(remap)
+    
+        # Create dummies
+        self.transf_df = self.transf_df.merge(pd.get_dummies(self.df["Legal form"]),
+                                        how="left", left_index=True,
+                                        right_index=True)
 
+    def df_to_csv(self):
+        self.df.to_csv("data/temp_df.csv")
 
-def add_status(df, new_df):
-    # Add status to new df
-    new_df["status"] = df["Legal situation"]
-    replace_map = {"Conclusion of liquidation": "Liquidation",
-                   "Reopening liquidation": "Liquidation",
-                   "Conclusion of bankruptcy": "Bankruptcy"}
-    new_df["status"] = new_df["status"].replace(replace_map)
-
-    # Calculate years to event
-    new_df["end_date"] = df["Legal situation date"]
-    new_df["start_date"] = df["Date of incorporation"]
-
-    new_df["end_date"] = pd.to_datetime(new_df["end_date"])
-    new_df["start_date"] = pd.to_datetime(new_df["start_date"])
-
-    new_df["years_to_event"] = [len(pd.date_range(start=i, end=j, freq="Y"))
-                                for i, j in zip(new_df["start_date"],
-                                                new_df["end_date"])]
-    new_df["years_to_event"] = new_df["years_to_event"].astype(int)
-
-    return new_df
-
-
-def add_event_indicator(new_df):
-    indicator_map = new_df["status"].unique().tolist()
-    indicator_map = {i:j for i, j in
-                     zip(indicator_map, list(range(len(indicator_map))))}
-
-    new_df["event_indicator"] = [indicator_map[i] for i in new_df["status"]]
-
-    return new_df
-
-
-def add_mean_pl(df, new_df):
-    pl_columns = [i for i in df.columns if "P/L" in i]
-    df[pl_columns] = df[pl_columns].replace("n.a.", np.nan)
-    new_df["mean_pl"] = df[pl_columns].fillna(0).mean(axis=1)
-    new_df["mean_pl"] = new_df["mean_pl"] / 1000
-    return new_df
-
-
-def add_mean_cash_flow(df, new_df):
-    cash_flow_columns = [i for i in df.columns if "Cash flow" in i]
-    df[cash_flow_columns] = df[cash_flow_columns].replace("n.a.", np.nan)
-    new_df["mean_cash_flow"] = df[cash_flow_columns].fillna(0).mean(axis=1)
-    return new_df
-
-
-def add_mean_total_assets(df, new_df):
-    total_assets_columns = [i for i in df.columns if "Total assets" in i]
-    df[total_assets_columns] = df[total_assets_columns].replace("n.a.", np.nan)
-    new_df["mean_total_assets"] = df[total_assets_columns].fillna(0).mean(axis=1)
-    new_df["mean_total_assets"] = new_df["mean_total_assets"] / 1000
-    return new_df
-
-
-def add_mean_profit_margin(df, new_df):
-    profit_margin_columns = [i for i in df.columns if "Profit margin" in i]
-    df[profit_margin_columns] = df[profit_margin_columns].replace("n.a.", np.nan)
-    new_df["mean_profit_margin"] = df[profit_margin_columns].fillna(0).mean(axis=1)
-    return new_df
-
-
-def add_mean_n_employees(df, new_df):
-    employees_columns = [i for i in df.columns if "employees" in i]
-    df[employees_columns] = df[employees_columns].replace("n.a.", np.nan)
-    new_df["mean_n_employees"] = df[employees_columns].fillna(0).mean(axis=1)
-    return new_df
-
-
-def add_mean_legal_events(df, new_df):
-    new_df["n_legal_events"] = df["Legal events - Date"].str.count("\n")
-    new_df["n_legal_events"] = new_df["n_legal_events"].fillna(0)
-    new_df["n_legal_events"] = new_df["n_legal_events"] + 1
-    return new_df
-
-
-def add_peer_group_size(df, new_df):
-    new_df["peer_group_size"] = df["Peer Group Size"]
-    return new_df
-
-
-def add_legal_form(df, new_df):
-    private_list = [
-        "Private company with limited liability", "private_company",
-        "Private company limited by shares",
-        "Starter Private company with limited liability",
-        "Limited partnership", "Partnership limited by shares",
-        "General partnership", "One-man company with limited liability",
-        "Private company with limited liability with a social aim",
-        "Private Founding",
-        "Professional partnership in the form of a private company with limited liability",
-        "Private company"
-    ]
-    public_list = [
-        "Company limited by shares",
-        "Public company limited by shares",
-        "Public company of another legal form",
-        "Public utility Founding",
-        "Public institution"
-    ]
-    cooperative_list = [
-        "Co-operative society with limited liability",
-        "Co-operative society with limited liability with a social aim",
-        "Co-operative society",
-        "Co-operative society with unlimited joint and several liability",
-        "Co-operative society with limited liability by way of participating interest",
-        "Public co-operative society"
-    ]
-    nonprofit_list = [
-        "Non-profit association", "Private non-profit association",
-        "Association in charge of missions",
-        "International non-profit association",
-        "Company limited by shares with a social aim"
-    ]
-    other_list = [
-        "Foreign company whose nature is not described",
-        "Provincial government control",
-        "Project Association",
-        "Autonomous local authority with legal personality",
-        "Economic joint venture with Belgian office",
-        "European economic joint venture with Belgian office",
-        "Public utility founding"
-    ]
-
-    categories =  [private_list, public_list, cooperative_list, nonprofit_list,
-                   other_list]
-    labels = ["private", "public", "cooperative", "nonprofit", "other"]
-
-    remap = {}
-    for i, j in zip(categories, labels):
-        for k in i:
-            remap[k] = j
-
-    df["Legal form"] = df["Legal form"].replace(remap)
-
-    # Create dummies
-    new_df = new_df.merge(pd.get_dummies(df["Legal form"]), how="left",
-                          left_index=True, right_index=True)
-
-    return new_df
+    def to_csv(self):
+        self.transf_df.to_csv("data/bankruptcy_transformed.csv")
 
 
 def main():
-    df = read_df(path="data/Bel-first_Export_2.txt")
-    df = fill_index(df)
-
-    print(df.columns)
-
-    new_df = pd.DataFrame(index=df.index.unique())
-    new_df.index = new_df.index.rename("index")
-
-    new_df = add_status(df, new_df)
-    new_df = add_event_indicator(new_df)
-
-    new_df = new_df[new_df["status"].isin(
-        ["Liquidation", "Bankruptcy", "Merger by absorption"]
-    )]
-
-    new_df = add_mean_pl(df, new_df)
-    new_df = add_mean_total_assets(df, new_df)
-    new_df = add_mean_n_employees(df, new_df)
-    new_df = add_legal_form(df, new_df)
-
-    df.to_csv("data/temp_df.csv")
-    new_df.to_csv("data/bankruptcy_transformed.csv", index=False)
+    dataset = BankruptcyDataset(path="data/Bel-first_Export_2.txt")
+    dataset.create_transf_df()
+    dataset.df_to_csv()
+    dataset.to_csv()
 
 
 if __name__=="__main__":
--- competing_risks.R
+++ competing_risks.R
@@ -2,24 +2,30 @@ library(survival)
 library(survminer)
 library(smcure)
 library(cmprsk)
-options(scipen=999)
+options(scipen = 999)
 
 data <- read.csv("data/bankruptcy_transformed.csv")
 data <- na.omit(data)
-data$censor <- ifelse(data$event_indicator==3, 1, 0)
-
-head(data)
-table(data$status, data$event_indicator)
-cat("\n")
+data$censor <- ifelse(data$event_indicator == 2, 1, 0)
 
 # Plot survival curve
-fit <- survfit(Surv(years_to_event, censor)~1, data=data)
-jpeg(file="visualizations/km_bankruptcy.jpeg")
-ggsurvplot(fit, data=data)
+fit <- survfit(Surv(years_to_event, censor)~1, data = data)
+jpeg(file = "visualizations/km_bankruptcy.jpeg")
+ggsurvplot(fit, data = data)
 dev.off()
 
 # Cox PH model
-# coxph(Surv(years_to_event, censor)~mean_pl+mean_total_assets, data=data)
+cox0 <- coxph(Surv(years_to_event, censor) ~ mean_pl + mean_total_assets
+             + mean_n_employees + cooperative + nonprofit + other + private,
+             data = data)
+
+coefficients_cox <- cox0$coefficients
+stand_e_cox <- sqrt(diag(cox0$var))
+p_value_cox <- 2 * (1 - pnorm(abs(coefficients_cox / stand_e_cox),
+                  lower.tail = TRUE))
+
+cox_summary <- data.frame(coefficients_cox, stand_e_cox, p_value_cox)
+write.csv(cox_summary, "data/cox_summary.csv")
 
 # Population mixture cure model
 # smcure(Surv(years_to_event, censor)~mean_pl+mean_total_assets,
@@ -29,30 +35,62 @@ dev.off()
 # Competing risk Cox PH on the subdistribution hazard
 cov <- cbind(data$mean_pl, data$mean_total_assets, data$mean_n_employees,
              data$cooperative, data$nonprofit, data$other, data$private)
-dimnames(cov)[[2]] = c("Mean profit/loss", "Mean total assets",
-                       "Mean number of employees", "Cooperative","Nonprofit",
-                       "Other", "Private")
+dimnames(cov)[[2]] <- c("Mean profit/loss", "Mean total assets",
+                        "Mean number of employees", "Cooperative", "Nonprofit",
+                        "Other", "Private")
 
-c0 <- crr(data$years_to_event, data$event_indicator, cov, failcode=0)
-c1 <- crr(data$years_to_event, data$event_indicator, cov, failcode=1)
-c2 <- crr(data$years_to_event, data$event_indicator, cov, failcode=2)
+c0 <- crr(data$years_to_event, data$event_indicator, cov, failcode = 0)
+c1 <- crr(data$years_to_event, data$event_indicator, cov, failcode = 1)
+c2 <- crr(data$years_to_event, data$event_indicator, cov, failcode = 2)
 
-pc0 <- predict(c0, rbind(c(0, 0, 0), c(1, 0, 0), c(0, 1, 0), c(0, 0, 1)))
-pc1 <- predict(c1, rbind(c(0, 0, 0), c(1, 0, 0), c(0, 1, 0), c(0, 0, 1)))
-pc2 <- predict(c2, rbind(c(0, 0, 0), c(1, 0, 0), c(0, 1, 0), c(0, 0, 1)))
+# Create a table of coefficients
+coeff_liquidation <- c0$coef
+se_liquidation <- sqrt(diag(c0$var))
+p_value_liquidation <- 2 * (1 - pnorm(abs(coeff_liquidation / se_liquidation),
+                                      lower.tail = TRUE))
 
-cat("Liquidation\n\n"); c0; cat("\n");
-cat("Merger\n\n"); c1; cat("\n");
-cat("Bankruptcy\n\n"); c2; cat("\n");
+coeff_merger <- c1$coef
+se_merger <- sqrt(diag(c1$var))
+p_value_merger <- 2 * (1 -  pnorm(abs(coeff_merger / se_merger),
+                                  lower.tail = TRUE))
 
-colors = c("black", "green", "blue", "red")
+coeff_bankruptcy <- c2$coef
+se_bankruptcy <- sqrt(diag(c2$var))
+p_value_bankruptcy <- 2 * (1 - pnorm(abs(coeff_bankruptcy / se_bankruptcy),
+                                     lower.tail = TRUE))
 
-jpeg(file="visualizations/temp.jpeg", width=2000, height=2000, res=200)
-par(mfrow=c(2, 2))
-plot(pc0, col=colors, lty=1, ylim=c(0,0.8), main="Liquidation",
-     xlab="Time",ylab="Probability")
-plot(pc1, col=colors, lty=1, ylim=c(0,0.8), main="Merger",
-     xlab="Time",ylab="Probability")
-plot(pc2, col=colors, lty=1, ylim=c(0,0.8), main="Bankruptcy",
-     xlab="Time",ylab="Probability")
+competing_summary <- data.frame(
+  coeff_liquidation, se_liquidation, p_value_liquidation,
+  coeff_merger, se_merger, p_value_merger,
+  coeff_bankruptcy, se_bankruptcy
+)
+write.csv(competing_summary, "data/competing_summary.csv")
+
+# Create a plot of survival probabilities
+pc0 <- predict(c0, rbind(c(0, 0, 0, 1, 0, 0, 0), c(0, 0, 0, 0, 1, 0, 0),
+                         c(0, 0, 0, 0, 0, 1, 0), c(0, 0, 0, 0, 0, 0, 1)))
+pc1 <- predict(c1, rbind(c(0, 0, 0, 1, 0, 0, 0), c(0, 0, 0, 0, 1, 0, 0),
+                         c(0, 0, 0, 0, 0, 1, 0), c(0, 0, 0, 0, 0, 0, 1)))
+pc2 <- predict(c2, rbind(c(0, 0, 0, 1, 0, 0, 0), c(0, 0, 0, 0, 1, 0, 0),
+                         c(0, 0, 0, 0, 0, 1, 0), c(0, 0, 0, 0, 0, 0, 1)))
+
+colors <- c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728")
+
+jpeg(file = "visualizations/temp.jpeg", width = 4000, height = 1500, res = 300)
+par(mfrow = c(1, 3))
+plot(pc0, col = colors, lty = 1, ylim = c(0, 1),
+     main = "Liquidation, relative to the public sector",
+     xlab = "Time", ylab = "Probability", lwd = 2)
+legend(0, 1, c("Cooperative", "Nonprofit", "Other", "Private"),
+       col = colors, lty = 1)
+plot(pc1, col = colors, lty = 1, ylim = c(0, 1),
+     main = "Merger, relative to the public sector",
+     xlab = "Time", ylab = "Probability", lwd = 2)
+legend(0, 1, c("Cooperative", "Nonprofit", "Other", "Private"),
+       col = colors, lty = 1)
+plot(pc2, col = colors, lty = 1, ylim = c(0, 1),
+     main = "Bankruptcy, relative to the public sector",
+     xlab = "Time", ylab = "Probability", lwd = 2)
+legend(0, 1, c("Cooperative", "Nonprofit", "Other", "Private"),
+       col = colors, lty = 1)
 dev.off()
